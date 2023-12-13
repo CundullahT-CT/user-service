428485acdd6f9e001657a0542b7b78fad21b88d5
CundullahT-CT <cterzioglu@cybertekschool.com>
2023-12-14 00:12:22 +0100

    updates clients with ec2 urls

--- src/main/java/com/cydeo/client/ProjectClient.java
+++ src/main/java/com/cydeo/client/ProjectClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(value = "project-service", url = "http://localhost:8082")
+@FeignClient(value = "project-service", url = "http://3.70.95.29:8082")
 public interface ProjectClient {
 
     @GetMapping("/api/v1/project/count/manager/{assignedManager}")

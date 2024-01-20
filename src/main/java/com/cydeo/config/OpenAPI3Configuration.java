26e4196168160877d5a638bd08a92b04f3961e3f
CundullahT-CT <cterzioglu@cybertekschool.com>
2024-01-20 17:44:46 +0100

    fixes keycloak + swagger, updates exception and response pojo names

--- src/main/java/com/cydeo/config/OpenAPI3Configuration.java
+++ src/main/java/com/cydeo/config/OpenAPI3Configuration.java
@@ -32,8 +32,8 @@ public class OpenAPI3Configuration {
 
         private Info getInfo() {
                 return new Info()
-                        .title("Ticketing App Rest")
-                        .description("Api documentation")
+                        .title("User API Doc")
+                        .description("API documentation for Ticketing App User Operations")
                         .version("v1.0");
         }
 
